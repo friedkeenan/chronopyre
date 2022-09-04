f0f33cc21c30a206f39fade20752519a197413ae
friedkeenan <friedkeenan@protonmail.com>
2022-09-04 16:13:00 -0500

    Have cats relax by the campfire their owner is resting at

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ This mod adds the ability to rest at campfires to pass the night instead of slee
 
 When it is night or thunderstorming, players may right click a lit campfire with an empty hand to begin resting at it. When enough players are resting, determined by the usual `playersSleepingPercentage` game rule, the advancement of time will speed up, turning a night that normally takes 10 minutes into one that takes 2. When all players using a campfire to rest have finished resting, the campfire will go out. To continue resting, players must stay near their campfire, or else their rest will be cancelled. Additionally, right clicking the campfire you are resting at with an empty hand will cancel your rest.
 
-Campfires have been made unlit by default in order to better balance resting. Beds have also been changed so that players cannot sleep in them. Don't worry though, resting at a campfire will stave off phantoms. However, it is currently impossible for cats to give their owner gifts when resting.
+Campfires have been made unlit by default in order to better balance resting. Beds have also been changed so that players cannot sleep in them, though they may still be used to set their respawn point. Don't worry though, resting at a campfire will stave off phantoms, and cats will relax by the fire when their owner is resting.
 
 Tent structures have also been added to the world generation, complete with a campfire and meager loot.
---
+++ src/main/java/io/github/friedkeenan/chronopyre/mixin/CatRelaxByCampfire.java
@@ -0,0 +1,103 @@
+package io.github.friedkeenan.chronopyre.mixin;
+
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import io.github.friedkeenan.chronopyre.Rester;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Cat;
+import net.minecraft.world.entity.player.Player;
+
+@Mixin(targets = {"net.minecraft.world.entity.animal.Cat$CatRelaxOnOwnerGoal"})
+public class CatRelaxByCampfire {
+    @Shadow
+    @Nullable
+    private Player ownerPlayer;
+
+    @Shadow
+    @Nullable
+    private BlockPos goalPos;
+
+    @Redirect(
+        at = @At(
+            value  = "INVOKE",
+            target = "Lnet/minecraft/world/entity/LivingEntity;isSleeping()Z"
+        ),
+
+        method = "canUse"
+    )
+    private boolean isSleepingOrRestingCanUse(LivingEntity entity) {
+        /* We already know the entity is a player so the cast is safe. */
+        return entity.isSleeping() || ((Rester) entity).isResting();
+    }
+
+    @Redirect(
+        at = @At(
+            value  = "INVOKE",
+            target = "Lnet/minecraft/world/entity/player/Player;isSleeping()Z"
+        ),
+
+        method = "canContinueToUse"
+    )
+    private boolean isSleepingOrRestingCanContinueToUse(Player player) {
+        return player.isSleeping() || ((Rester) player).isResting();
+    }
+
+    @Shadow
+    private boolean spaceIsOccupied() {
+        throw new AssertionError();
+    }
+
+    @Inject(
+        at = @At(
+            value  = "INVOKE",
+            target = "Lnet/minecraft/world/entity/player/Player;blockPosition()Lnet/minecraft/core/BlockPos;"
+        ),
+
+        method      = "canUse",
+        cancellable = true
+    )
+    private void relaxByFire(CallbackInfoReturnable<Boolean> info) {
+        /* We only want to customize logic for resting. */
+        if (this.ownerPlayer.isSleeping()) {
+            return;
+        }
+
+        final var rester = (Rester) this.ownerPlayer;
+
+        /* We know the player is resting. */
+        this.goalPos = rester.getRestingPos().get();
+
+        info.setReturnValue(!this.spaceIsOccupied());
+    }
+
+    @Redirect(
+        at = @At(
+            value  = "INVOKE",
+            target = "Lnet/minecraft/world/entity/player/Player;getSleepTimer()I"
+        ),
+
+        method = "stop"
+    )
+    private int hasAlwaysRestedLongEnough(Player player) {
+        return 100;
+    }
+
+    @Redirect(
+        at = @At(
+            value  = "INVOKE",
+            target = "Lnet/minecraft/world/entity/animal/Cat;randomTeleport(DDDZ)Z"
+        ),
+
+        method = "giveMorningGift"
+    )
+    private boolean disableTeleport(Cat cat, double x, double y, double z, boolean broadcast_event) {
+        return true;
+    }
+}
